de010e0985dde738391188b0a149c6d49cfbf381
adel djidjik <ea_djidjik@esi.dz>
2022-01-21 11:07:54 +0100

    remove role from delete method

--- src/modules/contacts/contacts.router.ts
+++ src/modules/contacts/contacts.router.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 
 import JwtHelper from '@servichain/middlewares/JwtHelper'
-import {EUserRole} from '@servichain/enums/EUserRole'
 import {ContactsController, ContactsService, generateSchema, updateSchema} from '@servichain/modules/contacts'
 
 const router = express.Router()
@@ -12,6 +11,6 @@ router.post('/', JwtHelper.middleware(), generateSchema, controller.generate)
 router.put('/:id', JwtHelper.middleware(), updateSchema, controller.updateProtected)
 router.get('/', JwtHelper.middleware(), controller.getAllByUser)
 router.get('/:id', JwtHelper.middleware(), controller.getByIdProtected)
-router.delete('/', JwtHelper.middleware([EUserRole.Admin]),controller.delete)
+router.delete('/:id', JwtHelper.middleware(),controller.delete)
 
 export default router
