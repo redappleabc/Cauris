14f692e2bbc3bb75578d2c9bee60408992345531
Guilhem <guilhem.amardeilh@servicoo.com>
2022-02-03 15:48:20 +0100

    FIX | misplaced bracket

--- src/modules/transactions/transaction.service.ts
+++ src/modules/transactions/transaction.service.ts
@@ -110,7 +110,7 @@ export class TransactionService extends ServiceProtected {
     const account = await this.retrieveAccountByAddress(userId, from)
     RPCHelper.setWallet(account)
     const gasFees = await RPCHelper.estimate(to, value, coin)
-    return new ValidResponse(EHttpStatusCode.OK, {fees: utils.formatUnits(gasFees, "18"}))
+    return new ValidResponse(EHttpStatusCode.OK, {fees: utils.formatUnits(gasFees, "18")})
   }
 
   public async send(userId: string, coinId: string, from: string, to: string, value: string) {
