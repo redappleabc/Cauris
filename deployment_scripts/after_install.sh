3c8f722397f72302cb1616db7a9c56f00d229e9d
redapple961129 <redapple961129@hotmail.com>
2022-07-04 22:53:17 +0900

    code deploy change

--- deployment_scripts/after_install.sh
+++ deployment_scripts/after_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/servichain-backend-dev
+sudo rm -rf node_modules
 sudo npm install
--- deployment_scripts/before_install.sh
+++ deployment_scripts/before_install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-FILE=/home/ubuntu/servichain-backend-dev/server.ts
-if test -f "$FILE"; then
-   echo "$FILE exists"
-   cd /home/ubuntu/
-   pm2 stop --silent server.ts
-   pm2 delete server.ts
-   killall -9 node
-else 
-    echo "$FILE does not exist."
-fi
+# FILE=/home/ubuntu/servichain-backend-dev/server.ts
+# if test -f "$FILE"; then
+#    echo "$FILE exists"
+#    cd /home/ubuntu/
+#    pm2 stop --silent server.ts
+#    pm2 delete server.ts
+#    killall -9 node
+# else 
+#     echo "$FILE does not exist."
+# fi
 cd /home/ubuntu/
 sudo rm -rf servichain-backend-dev
 sudo mkdir servichain-backend-dev
