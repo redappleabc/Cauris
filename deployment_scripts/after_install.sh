4746bd3d6a89390839b1a24f5c2ec9258a83a605
redapple961129 <redapple961129@hotmail.com>
2022-07-05 01:53:22 +0900

    force build

--- deployment_scripts/after_install.sh
+++ deployment_scripts/after_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source /home/ubuntu/.bash_profile
 cd /home/ubuntu/servichain-backend-dev
-sudo aws s3 cp s3://servichain-backend-dev-config/config  ./config  --recursive
+sudo mkdir config
+sudo aws s3 cp s3://servichain-backend-dev-config/config ./config --recursive
 sudo rm -rf node_modules
 sudo npm install
