4d0f650f3e6126294582ab3f4655bfad5bd877bb
Guilhem <guilhem.amardeilh@servicoo.com>
2022-09-21 15:23:21 +0200

    FIX | issue where the wrong config file would be used

--- deployment_scripts/after_install.sh
+++ deployment_scripts/after_install.sh
@@ -2,6 +2,6 @@
 source /home/ubuntu/.bash_profile
 cd /home/ubuntu/servichain-backend
 sudo mkdir config
-sudo aws s3 cp s3://servichain-backend-dev-config/config ./config --recursive
+sudo aws s3 cp s3://servichain-backend-prod-config/config ./config --recursive
 sudo rm -rf node_modules
 sudo npm install
